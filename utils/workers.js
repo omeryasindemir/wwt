642721ff3598805903e0a78994fe29ecab84cade
Taylan Utku Yılmaz <86522690+tuyilmaz@users.noreply.github.com>
2025-01-28 22:43:19 +0300

    Update workers.js

--- utils/workers.js
+++ utils/workers.js
@@ -12,10 +12,7 @@ const createAuthWorker = async (userid, tckn, password, cookie) => {
             switch (data.op) {
                 case 0:
                     await User.updateOne({ _id: userid }, { cookie: data.value });
-                    console.log(Object.keys(Workers[userid].auctionWorkers).length);
-                    console.log(Workers[userid]);
-                    for (const auctionId in Object.keys(Workers[userid].auctionWorkers)) {
-                        console.log(auctionId);
+                    for (const auctionId in Workers[userid].auctionWorkers) {
                         Workers[userid]['auctionWorkers'][auctionId].postMessage({ op: 0, cookie: data.value });
                     }
                     Workers[userid]['latestCookie'] = data.value;
